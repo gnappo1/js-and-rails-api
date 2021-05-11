b89bf484aba3f80e7ea742e378152c623b2b1aad
Matteo <gnappo.1@live.it>
2021-05-11 16:50:51 -0700

    Post lecture update

--- frontend/javascripts/index.js
+++ frontend/javascripts/index.js
@@ -4,6 +4,10 @@ const buttonNew = () => document.getElementById("button-new")
 const ulDiv = () => document.getElementById("list")
 const ul = () => document.getElementById("categories-list")
 const productForm = () => document.getElementById("product-form")
+const productName = () => document.getElementById("product-name")
+const productPrice = () => document.getElementById("product-price")
+const productDescription = () => document.getElementById("product-description")
+const productSelectCategory = () => document.getElementById("category_id")
 
 document.addEventListener("DOMContentLoaded", () => {
     buttonShow().addEventListener("click", handleClick)
@@ -64,7 +68,6 @@ const renderCategory = (category) => {
     a.id = `category-${category.id}`
     a.innerText = category.name
     a.href = "#"
-    a.alt = `${category.name}`
     a.addEventListener("click", (e) => renderProducts(e, category))
     h4.appendChild(a)
     ul().appendChild(h4)
@@ -78,9 +81,12 @@ const renderProducts = (e, category) => {
         lis.forEach((li) => li.remove())
     } else {
         category.products.forEach(element => renderProduct(element, category.id));
+
     }
+
 }
 const renderProduct = (product, catId) => {
+    // debugger
     const a = document.getElementById(`category-${catId}`)
     const li = document.createElement("li")
     a.dataset.catId = catId
@@ -88,17 +94,54 @@ const renderProduct = (product, catId) => {
         <strong class="product-name">${product.name}</strong>
         <span class="product-price">${product.price}</span>
         <span class="product-description">${product.description}</span><br>
-        <button class="edit-product" data-id="${product.id}">Update</button>
+        <button class="edit-product" data-id="${product.id}">Edit</button>
         <button class="delete-product" data-id="${product.id}">Delete</button>
     `
     a.parentNode.appendChild(li)
+    document.querySelector(`button.delete-product[data-id='${product.id}']`).addEventListener("click", handleDelete)
 }
 
-const handleSubmit = () => {
-    debugger
+const handleSubmit = (e) => {
+    e.preventDefault()
+    const data = {
+        name: productName().value,
+        description: productDescription().value,
+        price: productPrice().value,
+        category_id: productSelectCategory().value
+    }
+
+    fetch("http://localhost:3000/products", {
+        method: 'POST',
+        headers: {
+            "Content-Type": 'application/json'
+        },
+        body: JSON.stringify(data)
+    })
+    .then(resp => resp.json())
+    .then(json => handleCreateProduct(json))
 }
 
+const handleDelete = (e) => {
+    fetch(`http://localhost:3000/products/${e.target.dataset.id}`, {
+        method: 'DELETE',
+        headers: {
+            "Content-Type": 'application/json'
+        }
+    })
+    .then(resp => resp.json())
+    .then(json => {
+        e.target.parentNode.remove()
+        alert(json.message)
+    })
+}
+
+const handleCreateProduct = (product) => {
+    //make sure the categories are on the page
+    //make sure the new product will end up on the page
+    ul().children.length < 1 ? handleClick() : renderProduct(product, product.category.id)
+    productForm().reset()
+}
 
 const handleError = (error) => {
     console.log(error)
-}
+} 
