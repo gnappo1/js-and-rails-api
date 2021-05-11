e30d57150d9e5884279b8fc20c9cecccd7d3a1b8
Matteo <gnappo.1@live.it>
2021-05-11 11:31:16 -0700

    Adjust JSON response in serializers

--- app/serializers/category_serializer.rb
+++ app/serializers/category_serializer.rb
@@ -1,12 +1,13 @@
 class CategorySerializer < ActiveModel::Serializer
   # include FastJsonapi::ObjectSerializer
-  attributes :name
+  attributes :name, :id
   has_many :products, except: [:created_at, :updated_at]
 
   def products
     self.object.products.map do |p|
       {
         name: p.name,
+        id: p.id,
         price: p.price,
         description: p.description
       }
